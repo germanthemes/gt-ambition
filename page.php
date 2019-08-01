71bb214d6e8954af65b8655da9aca3baeda1ba0b
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-08-01 11:49:45 +0200

    added comments to pages

--- page.php
+++ page.php
@@ -20,6 +20,11 @@ while ( have_posts() ) :
 
 	get_template_part( 'template-parts/page/content', 'page' );
 
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) :
+		comments_template();
+	endif;
+
 endwhile;
 
 get_footer();
