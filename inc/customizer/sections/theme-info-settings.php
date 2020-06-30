e2b169945e9042a4e2b8952d06fd75e47dad69b8
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-06-30 12:03:08 +0200

    removed teaser for GT Blocks in Customizer

--- inc/customizer/sections/theme-info-settings.php
+++ inc/customizer/sections/theme-info-settings.php
@@ -29,18 +29,5 @@ function gt_ambition_customize_register_theme_info_settings( $wp_customize ) {
 			'priority' => 10,
 		)
 	) );
-
-	// Add GT Local Fonts control.
-	if ( ! class_exists( 'GermanThemes_Blocks' ) ) {
-		$wp_customize->add_control( new GT_Ambition_Customize_Plugin_Control(
-			$wp_customize, 'gt_blocks_plugin', array(
-				'label'       => esc_html__( 'GT Blocks', 'gt-ambition' ),
-				'description' => esc_html__( 'Install our free GT Blocks plugin to create a business-styled homepage in the Editor with just a few clicks.', 'gt-ambition' ),
-				'section'     => 'gt_ambition_section_theme_info',
-				'settings'    => array(),
-				'priority'    => 30,
-			)
-		) );
-	}
 }
 add_action( 'customize_register', 'gt_ambition_customize_register_theme_info_settings' );
